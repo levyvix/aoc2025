830ca5e601ddd9a5cbe914f541e4ad11407b0c10
Levy Nunes <levy.vix@gmail.com>
2025-12-05 22:10:42 -0300

    clean code

--- d5/p1.py
+++ d5/p1.py
@@ -3,15 +3,16 @@ from pathlib import Path
 
 file_name = "r.in"
 ranges, products = (Path(__file__).parent / file_name).read_text().split("\n\n")
-ranges = [r for r in ranges.splitlines()]
+# [[3,5], ...]
+ranges = [[int(r.split("-")[0]), int(r.split("-")[1])] for r in ranges.splitlines()]
 products = [p for p in products.splitlines()]
 
 res = 0
 for p in products:
     product_id = int(p)
-    for r in ranges:
-        init, end = r.split("-")
-        if product_id > int(init) and product_id <= int(end):
+    for init, end in ranges:
+        # init, end = int(init), int(end)
+        if init <= product_id <= end:
             res += 1
             break
 
