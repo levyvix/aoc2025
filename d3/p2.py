676c7b2752d9912d1c1346953146524dc3715c68
Levy Nunes <levy.vix@gmail.com>
2025-12-07 17:06:35 -0300

    day3 part2

--- d3/p2.py
+++ d3/p2.py
@@ -1,17 +1,27 @@
+from icecream import ic
 from pathlib import Path
 
-file_name = "test.txt"
+file_name = "real.txt"
 contents = (Path(__file__).parent / file_name).read_text().splitlines()
 
+jolts = []
 for b in contents:
     bcp = b[:]
     jolt = []
     for i in range(12):
         max_ = -1
-        remaining_batteries = bcp[: -(12 - len(jolt))] if len(bcp) < 11 else bcp
+        remaining_batteries = bcp[: -(11 - len(jolt))]
+        if len(jolt) >= 11:
+            remaining_batteries = bcp
         for n in remaining_batteries:
             n = int(n)
             max_ = max(max_, n)
         jolt.append(str(max_))
         index_max = bcp.index(str(max_))
         bcp = bcp[index_max + 1 :]
+    jolts.append(jolt)
+
+total = 0
+for j in jolts:
+    total += int("".join(j))
+ic(total)
