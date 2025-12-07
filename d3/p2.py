c192477ff318d40219fdd7cb088c73accf3c2ba9
Levy Nunes <levy.vix@gmail.com>
2025-12-07 17:23:54 -0300

    comact

--- d3/p2.py
+++ d3/p2.py
@@ -13,11 +13,9 @@ for b in contents:
         remaining_batteries = bcp[: -(11 - len(jolt))]
         if len(jolt) >= 11:
             remaining_batteries = bcp
-        for n in remaining_batteries:
-            n = int(n)
-            max_ = max(max_, n)
-        jolt.append(str(max_))
-        index_max = bcp.index(str(max_))
+        max_ = max(remaining_batteries)
+        jolt.append(max_)
+        index_max = bcp.index(max_)
         bcp = bcp[index_max + 1 :]
     jolts.append(jolt)
 
