9d0bfb17333e1bb5fa9eea63a7e46bc88ff4341d
Levy Nunes <levy.vix@gmail.com>
2025-12-08 22:00:49 -0300

    main

--- d1/p1.py
+++ d1/p1.py
@@ -1,16 +1,21 @@
 from icecream import ic
 
-with open("r1.txt") as f:
-    lines = f.readlines()
-nzero = 0
-curr = 50
-for l in lines:
-    direction, times = l[0], l[1:]
-    if direction == "L":
-        curr = (curr - int(times)) % 100
-    else:
-        curr = (curr + int(times)) % 100
-    ic(curr)
-    if curr == 0:
-        nzero += 1
-print(nzero)
+
+def main():
+    with open("r1.txt") as f:
+        lines = f.readlines()
+    nzero = 0
+    curr = 50
+    for line in lines:
+        direction, times = line[0], line[1:]
+        if direction == "L":
+            curr = (curr - int(times)) % 100
+        else:
+            curr = (curr + int(times)) % 100
+        if curr == 0:
+            nzero += 1
+    ic(nzero)
+
+
+if __name__ == "__main__":
+    main()
