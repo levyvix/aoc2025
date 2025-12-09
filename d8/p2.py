d5a188770c20304511faf3128253a505a0f63771
Levy Nunes <levy.vix@gmail.com>
2025-12-08 23:08:17 -0300

    seila

--- d8/p2.py
+++ d8/p2.py
@@ -5,7 +5,6 @@ boxes = [l.split(",") for l in content.splitlines()]
 def euclidian_distance(box1, box2):
     X1, Y1, Z1 = map(int, box1)
     X2, Y2, Z2 = map(int, box2)
-    # Squared Euclidean distance (order preserved, avoids floating point)
     return (X1 - X2) ** 2 + (Y1 - Y2) ** 2 + (Z1 - Z2) ** 2
 
 
@@ -15,10 +14,8 @@ for i in range(len(boxes)):
         distance = euclidian_distance(boxes[i], boxes[j])
         distances[(i, j)] = distance
 
-# Sort distances by value
 distances = sorted(distances.items(), key=lambda item: item[1])
 
-# Union-Find structure
 parent = list(range(len(boxes)))
 size = [1] * len(boxes)
 
@@ -46,18 +43,15 @@ def union(x, y):
     return True
 
 
-# Keep connecting until there's only 1 cluster
 last_i, last_j = None, None
 for (i, j), distance in distances:
     if union(i, j):
         last_i, last_j = i, j
-        # Check if all boxes are now in the same cluster
         root = find(0)
         all_connected = all(find(box) == root for box in range(len(boxes)))
         if all_connected:
             break
 
-# Get X coordinates of the last two connected boxes
 x_i = int(boxes[last_i][0])
 x_j = int(boxes[last_j][0])
 print(x_i * x_j)
